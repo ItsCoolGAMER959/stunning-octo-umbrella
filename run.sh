4a8c5bea2091b2ef4eccfc9cad14dccefea2c496
ItsCoolGAMER959 <53452072+ItsCoolGAMER959@users.noreply.github.com>
2022-09-29 00:17:33 +0100

    commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+gh repo clone ItsCoolGAMER959/stunning-octo-umbrella
+cd stunning-octo-umbrella
+cp index.html /var/www/html/
+cp maths.html /var/www/html/Directories
+cp timestables.pdf "/var/www/html/Knowledge Organisers/"
+cp maths_core.pdf "/var/www/html/Knowledge Organisers/"
+cp maths_advanced.pdf "/var/www/html/Knowledge Organisers/"
+cd ..
+rm -rf stunning-octo-umbrella
